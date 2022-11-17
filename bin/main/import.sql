6ba82b813efea0d090d8e4960b33f9d49252d705
Miguel Morán <miguelrmor@gmail.com>
2022-11-16 20:12:31 -0600

    add: se agregan usuarios de los devs

--- bin/main/import.sql
+++ bin/main/import.sql
@@ -8,3 +8,9 @@ INSERT INTO `producto`( `nombre_producto`, `precio`, `descripcion`, `id_categori
 INSERT INTO `producto`( `nombre_producto`, `precio`, `descripcion`, `id_categoria`, `existencias`) VALUES ('Pintura De Oso 2', '55', 'Pintura con técnica sobre oleo', '2','1');	
 INSERT INTO `producto`( `nombre_producto`, `precio`, `descripcion`, `id_categoria`, `existencias`) VALUES ('Pintura De Oso 4', '55', 'Pintura con técnica sobre oleo', '2','1');	
 
+INSERT INTO `usuario`(`email`, `full_name`, `password`, `telephone`) VALUES ("karlafer6@hotmail.com", "Karla Fernanda Rodríguez Palapa", "Osito123", "5568795574");
+INSERT INTO `usuario`(`email`, `full_name`, `password`, `telephone`) VALUES ("odreygj@gmail.com", "Audrey Gerardo Juárez", "Osito123", "2223566325");
+INSERT INTO `usuario`(`email`, `full_name`, `password`, `telephone`) VALUES ("gabelardo04@gmail.com", "Abelardo Gutiérrez López", "Osito123", "9541239911");
+INSERT INTO `usuario`(`email`, `full_name`, `password`, `telephone`) VALUES ("vivancomiguel119@gmail.com", "Miguel Angel Vivanco Maldonado", "Osito123", "3781185488");
+INSERT INTO `usuario`(`email`, `full_name`, `password`, `telephone`) VALUES ("betancourt.lucero@gmail.com", "Lucero Clarisa Betancourt García", "Osito123", "5539192506");
+INSERT INTO `usuario`(`email`, `full_name`, `password`, `telephone`) VALUES ("miguelrmor@gmail.com", "Miguel Rodríguez Morán", "Osito123", "3310126351");
